1a62563a3cb57fcd7e0756c2be15ee3c89e88aec
ducky <asqrybot@gmail.com>
2026-08-03 23:42:31 -0400

    feat: ensure auth on refresh

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,5 +1,11 @@
+import { refreshAll } from "$app/navigation";
+import { PUBLIC_TWITCH_CHANNEL_ID } from "$env/static/public";
+import { refreshToken } from "$lib/auth";
 import { createAppConfig, getAppConfig } from "$lib/server/db/appConfig";
+import { getSession } from "$lib/server/db/sessions";
+import { getModeratedChannels, getUserFromToken } from "$lib/twitch";
 import type { Handle } from "@sveltejs/kit";
+import { setUserData } from "duckylib";
 
 export const handle: Handle = async ({ event, resolve }) => {
   let appState = getAppConfig();
@@ -16,6 +22,43 @@ export const handle: Handle = async ({ event, resolve }) => {
       "Content-Type": "audio/mp3",
       "Cache-Control": "no-store",
     });
+
+  let session = getSession();
+  if (
+    !event.cookies.get("token-0") ||
+    event.cookies.get("token-0") !== session?.access_token
+  ) {
+    if (session) {
+      if (session.user_id === PUBLIC_TWITCH_CHANNEL_ID) {
+        event.cookies.set("token-0", session.access_token, { path: "/" });
+        if (session.refresh_token)
+          event.cookies.set("token-r", session.refresh_token, { path: "/" });
+      }
+    }
+  } else if (event.cookies.get("token-0")) {
+    const tokenUser = await getUserFromToken(
+      event.cookies.get("token-0") || null,
+    );
+    if (!tokenUser) {
+      let rt = event.cookies.get("token-r");
+      if (rt) {
+        const refreshed = await refreshToken(rt);
+        if (refreshed) {
+          event.cookies.set("token-0", refreshed.access_token, { path: "/" });
+          if (refreshed.refresh_token)
+            event.cookies.set("token-r", refreshed.refresh_token, {
+              path: "/",
+            });
+        }
+      } else {
+        event.cookies.delete("token-0", { path: "/" });
+        try {
+          setUserData(null);
+          await refreshAll();
+        } catch (e) {}
+      }
+    }
+  }
 
   const response = await resolve(event);
   return response;
