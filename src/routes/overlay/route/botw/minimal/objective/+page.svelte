3f5457013703424504ba2dfffd35fca5206e2f20
ducky <asqrybot@gmail.com>
2026-09-11 18:34:59 -0400

    feat: custom objective overlay fr this time

---
+++ src/routes/overlay/route/botw/minimal/objective/+page.svelte
@@ -0,0 +1,347 @@
+<script lang="ts">
+    import { browser } from "$app/environment";
+    import { page } from "$app/state";
+    import { PUBLIC_TWITCH_CHANNEL_ID } from "$env/static/public";
+    import questIcon from "$lib/assets/quest_icon.png";
+    import { AppConfig } from "$lib/config";
+    import type { ApiResponse, DBCompletableObjective, DBObjective, DBRoute, DBRouteProgress } from "$lib/types";
+    import { onMount } from "svelte";
+
+    // let route: DBRoute | null = $state(null);
+    // let progress: DBRouteProgress | null = $state(null);
+    // let completed: DBCompletableObjective[] = $state([]);
+
+    // const routeCategories = [
+    //     { id: "great-plateau", order: 1 },
+    //     { id: "seeking-guidance", order: 2 },
+    //     { id: "zoras-domain", order: 3 },
+    //     { id: "goron-city", order: 4 },
+    //     { id: "rito-village", order: 5 },
+    //     { id: "gerudo-town", order: 6 },
+    //     { id: "masters-sword-and-past", order: 7 },
+    //     { id: "confronting-the-calamity", order: 8 },
+    //     { id: "shrines-and-collectibles", order: 9 },
+    // ];
+
+    // async function fetchRoute(): Promise<DBRoute | null> {
+    //   const res: ApiResponse<DBRoute | null> = await (await fetch(`/api/route/botw`)).json();
+    //   return res.data || null;
+    // }
+
+    // async function fetchProgress(): Promise<DBRouteProgress | null> {
+    //   const res: ApiResponse<DBRouteProgress | null> = await (await fetch(`/api/route/botw/progress/${PUBLIC_TWITCH_CHANNEL_ID}`)).json();
+    //   return res.data || null;
+    // }
+
+    // async function fetchCompleted(): Promise<DBCompletableObjective[]> {
+    //   const res: ApiResponse<DBCompletableObjective[] | null> = await (await fetch(`/api/route/botw/progress/${PUBLIC_TWITCH_CHANNEL_ID}/objectives/completed`)).json();
+    //   return res.data || [];
+    // }
+
+    // async function completeObjective(objectiveId: string): Promise<void> {
+    //   await fetch(`/api/route/botw/progress/${PUBLIC_TWITCH_CHANNEL_ID}/objectives/${objectiveId}/complete`, {method: "POST"})
+    // }
+
+    // async function unCompleteObjective(objectiveId: string): Promise<void> {
+    //   await fetch(`/api/route/botw/progress/${PUBLIC_TWITCH_CHANNEL_ID}/objectives/${objectiveId}/uncomplete`, {method: "POST"})
+    // }
+
+    // let currentCategory: string = $state("great-plateau");
+    // let currentObjective: DBObjective | null = $state(null);
+    // let nextObjective: DBObjective | null = $state(null);
+
+    // let featuredObjectives: string[] = $state([]);
+
+    // let lastCompleted: DBObjective | null = $state(null);
+    // let showingCompleted = $state(false);
+
+    // function setMostRecents() {
+    //   let nonCompleted = (route?.objectives || []).filter(o => !completed.some(c => c.objective_id === o.objective_id)).sort((a, b) => (routeCategories.find(c => c.id === a.category_id)?.order || 0) - (routeCategories.find(c => c.id === b.category_id)?.order || 0));
+    //   let recent = nonCompleted[0];
+    //   let next = nonCompleted[1];
+
+    //   currentCategory = recent.category_id;
+    //   currentObjective = recent;
+    //   nextObjective = next;
+
+    //   let lc = null;
+    //   if(!lastCompleted) lc = completed.filter(c => !featuredObjectives.includes(c.objective_id)).sort((a, b) => new Date(b.completed_at).getTime() - new Date(a.completed_at).getTime())?.[0] || null;
+    //   if(lc) lastCompleted = route?.objectives.find(o => o.objective_id === lc.objective_id) || null;
+    //   if(lastCompleted && !featuredObjectives.includes(lastCompleted.objective_id)) {
+    //     featuredObjectives.push(lastCompleted.objective_id);
+    //     if(browser) window.localStorage.setItem("featured_objectives", JSON.stringify(featuredObjectives))
+    //   }
+
+    //   if(!showingCompleted && lastCompleted) {
+    //     showingCompleted = true;
+    //     setTimeout(() => {
+    //       lastCompleted = null;
+    //       setTimeout(() => {
+    //         showingCompleted = false;
+    //       },5e2)
+    //     },10e3);
+    //   }
+    // }
+    //
+    let objective: string = $state(page.url.searchParams.get("objective") || "none");
+    let description: string = $state(page.url.searchParams.get("description") || "none");
+
+    let completed = $state(page.url.searchParams.get("complete") === "true");
+
+    onMount(async () => {
+      objective = page.url.searchParams.get("objective") || "none";
+      description = page.url.searchParams.get("description") || "none";
+      completed = page.url.searchParams.get("completed") === "true";
+      // if(browser) featuredObjectives = JSON.parse(window.localStorage.getItem("featured_objectives") || "[]");
+      // route = await fetchRoute();
+      // progress = await fetchProgress();
+      // completed = await fetchCompleted();
+      // setMostRecents();
+
+      // setInterval(async () => {
+      //   route = await fetchRoute();
+      //   progress = await fetchProgress();
+      //   completed = await fetchCompleted();
+      //   setMostRecents();
+      // },1e3)
+    })
+</script>
+
+    <div class="container">
+{#if objective !== "none"}
+
+            <div class="top notif-in" style:padding-bottom={completed ? "0" : "20px"}>
+                <div class="column">
+                    <div class="row">
+                        <div class="row">
+                            <img id="icon" src={questIcon} alt="">
+                            <h1>OBJECTIVE{completed ? " COMPLETE!" : ""}</h1>
+                        </div>
+                        <div class="row">
+                            <h3 style:color="var(--text)">{objective.toUpperCase()}</h3>
+                        </div>
+                    </div>
+                        {#if description !== "none" && !completed}
+                            <div class="row">
+                                <p class='description'>{description.toUpperCase()}</p>
+                            </div>
+                        {/if}
+                </div>
+
+            </div>
+
+            <!-- <div class="bottom">
+                <h3>{currentObjective?.category_name.toUpperCase()}</h3>
+                <h2 class="objective">{currentObjective?.name.toUpperCase()}</h2>
+                <p class="description">{currentObjective?.description.toUpperCase()}</p>
+            </div> -->
+
+{/if}
+</div>
+
+<style>
+    @import url("https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap");
+
+    /*BOTW special font*/
+    @font-face {
+        font-family: "triforce";
+        src: url("$lib/assets/font/breath_of_the_wild.otf") format("opentype");
+        font-weight: normal;
+        font-style: normal;
+    }
+
+    .container {
+        width: 100vw;
+        height: 100vh;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        font-family: "triforce";
+        text-align: center;
+    }
+
+    :root {
+        --bg-dark: #423c27f0;
+        --bg-dark-full: #423c27;
+        --bg: #564e30;
+        --border: green;
+
+        --text-effect: #FFE460;
+        --text-faded: #FCC413d3;
+
+        --text-dark: #ffffffab;
+        --text: #F5FAFA;
+
+    }
+
+    h1 {
+        letter-spacing: 2px;
+        font-size: 1.66em;
+        color: var(--text-effect);
+    }
+
+    h1#large {
+        font-size: 1.88em;
+    }
+
+    h2 {
+        font-size: 1.66em;
+        color: var(--text-dark);
+        letter-spacing: 2px !important;
+    }
+
+    .objective {
+        color: var(--text-effect);
+        /*text-shadow: 0px 0px 2px var(--text-faded);*/
+        letter-spacing: 3px;
+    }
+
+    .description {
+        font-size: 1.1em;
+        text-shadow: 0px 0px 5px #4FC0FF;
+        font-family: "Roboto", sans-serif;
+        font-weight: 700;
+        font-style: italic;
+    }
+
+    h3 {
+        width: 100%;
+        font-size: 1.66em;
+        color: var(--text-dark);
+        letter-spacing: 1px !important;
+    }
+
+    p {
+        width: 80%;
+        font-size: 1.33em;
+        color: var(--text);
+        display: -webkit-box;
+        -webkit-line-clamp: 1;
+        -webkit-box-orient: vertical;
+        overflow: hidden;
+    }
+
+    .overlay {
+        margin: 10px;
+        width: 90%;
+        height: fit-content;
+        aspect-ratio: 20/8 !important;
+        background-color: var(--bg-dark);
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: flex-start;
+
+        border-radius: 8px;
+    }
+
+    .top {
+        box-shadow: 0px 0px 30px 3px var(--bg-dark);
+        width: 90%;
+        background-color: var(--bg);
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        justify-content: center;
+        border-radius: 8px;
+        text-align: left !important;
+        background: linear-gradient(to bottom,
+        var(--bg),
+        var(--bg-dark-full)
+        );
+    }
+
+    .row {
+        width: 100%;
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        justify-content: center;
+        gap: 0.33em;
+    }
+
+    .column {
+        width: 100%;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        gap: 0.33em;
+        text-align: center;
+    }
+
+    @keyframes fadeIn {
+        from {
+            opacity: 0;
+        }
+
+        to {
+            opacity: 1;
+        }
+    }
+
+    @keyframes fadeOut {
+        from {
+            opacity: 1;
+        }
+
+        to {
+            opacity: 0;
+        }
+    }
+
+    @keyframes grow {
+        0% {
+            transform: scale(1);
+        }
+
+        250% {
+            transform: scale(1.1);
+        }
+
+        50% {
+            transform: scale(1);
+        }
+
+        75% {
+            transform: scale(1.1);
+        }
+
+        100% {
+            transform: scale(1);
+        }
+    }
+
+    .growing {
+        animation: grow 5s linear infinite;
+    }
+
+
+    .notif-in {
+        animation: 1s fadeIn ease-in forwards;
+    }
+
+    .notif-out {
+        animation: 1s fadeOut ease-in forwards;
+    }
+
+    .notif {
+        transition: all;
+        position: absolute;
+        margin: 10px;
+        width: 90%;
+        aspect-ratio: 20/8 !important;
+        background-color: var(--bg-dark-full);
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+
+        border-radius: 8px;
+        box-shadow: 0px 0px 30px 3px var(--bg-dark);
+
+        gap: 3em;
+
+    }
+</style>
